b74e8eb1683db4ade605cd2304d4bf5a4a786fa1
iraviteja4u <raviteja_4455@outlook.com>
2021-06-23 14:00:09 +0530

    Changes in the hello world java file

--- src/com/teja/Hello_World.java
+++ src/com/teja/Hello_World.java
@@ -3,5 +3,6 @@ package com.teja;
 public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello, i'm trying new things");
     }
 }
